b02ca5c290771a8c6cbef687b3212054b48ed5da
Sebastian David <sebastian_bdavid@proton.me>
2026-03-03 17:43:04 -0500

    created user model

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -14,6 +14,7 @@ public class AppDbContext : DbContext
     public DbSet<NbaGame> NbaGames { get; set; }
     public DbSet<NbaPlayer> NbaPlayers { get; set; }
     public DbSet<NbaPlayerGameStats> NbaPlayerGameStats { get; set; }
+    public DbSet<User>Users { get; set; }
     
     // TODO: Create models for other tables (users, transactions,
     // TODO: transaction_details, leagues, fantasy_teams, fantasy_rosters, draft_picks, fantasy_scores)
@@ -21,5 +22,9 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
     }
 }
